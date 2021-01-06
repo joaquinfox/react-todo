bd53904a7e4eea990a9b6cd9ef54a41a1d50fb42
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-01-06 12:55:55 -0700

    placeholder and clear button

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 1 render an input form
-2 capture inputs and render them in a list
+2 capture inputs in state
+3 create a state list of items and render the list
+4 add edit and delete buttons to each rendered item
--- src/App.js
+++ src/App.js
@@ -11,14 +11,25 @@ function App() {
     <main>
       <section>
         <form onSubmit={handleSubmit} action="">
-          <input onChange={(e) => setItemName(e.target.value)} type="text" />
+          <input
+            onChange={(e) => setItemName(e.target.value)}
+            type="text"
+            placeholder="e.g. eggs"
+          />
           <button>click</button>
         </form>
       </section>
       <section>
-        {list.map((item) => {
-          return <li>{item}</li>;
+        {list.map((item, index) => {
+          return (
+            <li key={index}>
+              {item}
+              <button>edit</button>
+              <button>delete</button>
+            </li>
+          );
         })}
+        <button>clear list</button>
       </section>
     </main>
   );
