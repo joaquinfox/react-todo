b80da6a4dc0e4a14bf73c184af2f4c18cdb15c28
Joaquin Fox <Joaquin.Fox@gmail.com>
2021-01-06 12:32:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section>
+        <form action="">
+          <input type="text" />
+          <button>click</button>
+        </form>
+      </section>
+      <section>{/* the list goes here */}</section>
+    </main>
   );
 }
 
